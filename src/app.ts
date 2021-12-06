765f4904afeb6622dd87e0e64a77794396d16512
hkovesdi <kovesdi.hunor99@gmail.com>
2021-12-06 12:41:19 +0100

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import { json } from 'body-parser';
+import { router } from './routes';
+import { onlyAcceptJson } from './middlewares/onlyAcceptJson';
+import mongoose from 'mongoose';
+import { errorHandler } from './middlewares/errorHandler';
+
+async function main() {
+  dotenv.config();
+
+  const app = express();
+
+  app.use(json());
+  app.use(onlyAcceptJson);
+
+  app.use(router);
+
+  app.use(errorHandler);
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI ?? "");
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(process.env.PORT, () => {
+    console.log(`App is running on port ${process.env.port}`);
+  });
+}
+
+main();
