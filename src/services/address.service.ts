b99e99f7a214aa34884774d52cf82629aa92a357
hkovesdi <kovesdi.hunor99@gmail.com>
2021-12-06 12:40:27 +0100

    Create address service for encapsulating business logic

---
+++ src/services/address.service.ts
@@ -0,0 +1,61 @@
+import { ResponseError } from "../errors/responseError";
+import addressModel, { AddressStatus } from "../models/address.model";
+
+export async function getAddresses() {
+  const addresses = await addressModel.find();
+  return addresses;
+}
+
+export async function getLastUpdatedAddressDate() {
+  const address = await addressModel.findOne().sort({updatedAt: 'desc'});
+  
+  // If the database is empty Thu, 01 Jan 1970 00:00:00 GMT will be returned
+  let updatedAt = new Date(0).toUTCString();
+  
+  if(address) {
+    updatedAt = new Date(address.updatedAt).toUTCString();
+  }
+
+  return updatedAt;
+}
+
+export async function getAddressById(id: string) {
+  const address = await addressModel.findById(id);
+  
+  if(!address) {
+    throw new ResponseError("The requested address could not be found", 404);
+  }
+
+  return address;
+}
+
+export async function createAddress(data: any) {
+  const address = await addressModel.create(data);
+
+  return address;
+}
+
+export async function updateAddress(id: string, data: any) {
+  const address = await getAddressById(id);
+  
+  if(
+    address.status === AddressStatus.NOT_INTERESTED 
+    || address.status === AddressStatus.INTERESTED
+  ) {
+    throw new ResponseError("The address can only be modified if status is either 'null' or 'not at home'", 403);
+  }
+
+  return await addressModel.findByIdAndUpdate(address._id, data, {returnDocument: 'after'});
+}
+
+export async function deleteAddress(id: string) 
+{
+  const address = await getAddressById(id);
+
+  try {
+    await addressModel.findByIdAndDelete(address._id);
+  }
+  catch {
+    throw new ResponseError("The address can only be modified if status is either 'null' or 'not at home'", 409);
+  }
+}
