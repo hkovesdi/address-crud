a37a83538a86c130a387a68955b199e11b66ac32
hkovesdi <kovesdi.hunor99@gmail.com>
2021-12-06 12:39:29 +0100

    Create responseError custom error

---
+++ src/errors/responseError.ts
@@ -0,0 +1,8 @@
+export class ResponseError extends Error {
+  public status: number;
+  constructor(message: string, status: number) {
+    super();
+    this.message = message;
+    this.status = status;
+  }
+}
